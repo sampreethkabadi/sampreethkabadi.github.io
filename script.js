3f2aa7841ae8e182f2c304743c96111845397e23
sampreethkabadi <samkabadi10@gmail.com>
2025-02-09 00:32:43 +0530

    Initial portfolio changes.

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-function toggleDarkMode() {
-    document.body.classList.toggle("dark-mode");
-
-    // Save the preference in localStorage
-    if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("darkMode", "enabled");
-    } else {
-        localStorage.setItem("darkMode", "disabled");
-    }
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
-
-// Apply saved theme preference on page load
-window.onload = function() {
-    if (localStorage.getItem("darkMode") === "enabled") {
-        document.body.classList.add("dark-mode");
-    }
-};
